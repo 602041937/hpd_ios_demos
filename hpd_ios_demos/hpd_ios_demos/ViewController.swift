ec7202bf839515454d87468f257f2123e130b17d
HPD <602041937@qq.com>
2019-09-03 22:28:53 +0800

    初始化项目

---
+++ hpd_ios_demos/hpd_ios_demos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hpd_ios_demos
+//
+//  Created by hpd on 2019/9/3.
+//  Copyright © 2019 hpd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hpd_ios_demos/hpd_ios_demosTests/hpd_ios_demosTests.swift
@@ -0,0 +1,34 @@
+//
+//  hpd_ios_demosTests.swift
+//  hpd_ios_demosTests
+//
+//  Created by hpd on 2019/9/3.
+//  Copyright © 2019 hpd. All rights reserved.
+//
+
+import XCTest
+@testable import hpd_ios_demos
+
+class hpd_ios_demosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
