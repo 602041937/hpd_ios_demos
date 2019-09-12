48eaad5aec456e46e25af1c9a36ce38f488d5fc6
huangpeidong <602041937@qq.com>
2019-09-12 17:14:08 +0800

    cardView

--- hpd_ios_demos/hpd_ios_demos/AppDelegate.swift
+++ hpd_ios_demos/hpd_ios_demos/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let nav = UINavigationController(rootViewController: AutoWrapViewController.newInstance())
+        let nav = UINavigationController(rootViewController: CardViewController.newInstance())
         window?.rootViewController = nav
         
         return true
