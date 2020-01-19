01d7e23c1f94db6cbe2ac7c514cfc61495ec85a4
HPD <602041937@qq.com>
2020-01-19 15:26:39 +0800

    懒加载控制器 完成

---
+++ hpd_ios_demos/hpd_ios_demos/布局/懒加载控制器/Page1ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  Page1ViewController.swift
+//  hpd_ios_demos
+//
+//  Created by hpd on 2020/1/19.
+//  Copyright © 2020 hpd. All rights reserved.
+//
+
+import UIKit
+
+class Page1ViewController: HCPLazyLoadViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .yellow
+    }
+}
---
+++ hpd_ios_demos/hpd_ios_demos/布局/懒加载控制器/Page2ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  Page2ViewController.swift
+//  hpd_ios_demos
+//
+//  Created by hpd on 2020/1/19.
+//  Copyright © 2020 hpd. All rights reserved.
+//
+
+import UIKit
+
+class Page2ViewController: HCPLazyLoadViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
