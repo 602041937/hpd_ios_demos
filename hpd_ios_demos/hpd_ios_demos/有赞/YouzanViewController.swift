f32f653cd2d45ac89ce79abcc06b77116f910d00
hcp <602041937@qq.com>
2021-05-27 14:47:22 +0800

    调试有赞SDK

--- hpd_ios_demos/hpd_ios_demos/有赞/YouzanViewController.swift
+++ hpd_ios_demos/hpd_ios_demos/有赞/YouzanViewController.swift
@@ -11,24 +11,77 @@ import YZBaseSDK
 
 class YouzanViewController: UIViewController {
     
-    private let webView = YZWebView()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         let config = YZConfig.init(clientId: "d259ac4afc33521d0e", andAppKey: "a270386882e34007b25869ed2f4e2f5f")
         config.scheme = "hpddemo"
+        YZSDK.shared.delegate = self
         YZSDK.shared.initializeSDK(with: config)
         
         view.backgroundColor = .yellow
-
-        view.addSubview(webView)
-        
-        webView.frame = CGRect(x: 0, y: 0, width: 300, height: 600)
-        
-      
-        let url = URL(string: "https://www.baidu.com")
-        let request =  NSURLRequest.init(url: url!)
-        webView.load(request as URLRequest)
     }
 }
+
+
+extension YouzanViewController: YZSDKDelegate {
+    
+    func yzsdkSecurityCheckSucceed(_ sdk: YZSDK) {
+        print("youzan yzsdkSecurityCheckSucceed")
+    
+        DispatchQueue.main.async {
+            let webView = YZWebView()
+            self.view.addSubview(webView)
+            webView.noticeDelegate = self
+            webView.snp.makeConstraints { make in
+                make.left.right.bottom.equalToSuperview()
+                make.top.equalTo(70)
+            }
+            let url = URL(string: "https://shop41610176.m.youzan.com/wscgoods/detail/277r8pfd8gmeg")
+            let request =  NSURLRequest.init(url: url!)
+            webView.load(request as URLRequest)
+        }
+    }
+    
+    func yzsdkSecurityCheckFaild(_ sdk: YZSDK) {
+        print("youzan yzsdkSecurityCheckFaild")
+    }
+    
+}
+
+extension YouzanViewController: YZWebViewNoticeDelegate {
+ 
+    func webView(_ webView: YZWebViewProtocol, didReceive notice: YZNotice) {
+        print("youzan didReceive = \(notice)")
+        switch (notice.type) {
+        case .login: // 收到登陆请求
+            // 此时应调用SDK的登录API
+            YZSDK.shared.login(withOpenUserId: "10086", avatar: nil, extra: nil, nickName: nil, gender: 0) { isSuccess, openId in
+                print("youzan isSuccess=\(isSuccess),openId=\(openId)")
+                
+                DispatchQueue.main.async {
+                    webView.reload()
+                }
+            }
+            break;
+            
+        case .share: // 收到分享的回调数据
+            
+            // 在这里自定义分享操作
+            break;
+            
+        case .ready: // Web页面已准备好，可以调用分享接口
+            
+            // 这里可以启用分享按钮（如果有的话）
+            break;
+            
+        case .paymentFinished: // 付款完成时，回调
+            print("youzan 付款完成，可以做一些操作")
+            break;
+        default:
+            break;
+        }
+    }
+}
+
